3d030e0128799adefac51cf1a2c1a6f970013dc7
idunnoiam <74386219+idunnoiam@users.noreply.github.com>
2022-04-29 21:12:53 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -242,6 +242,40 @@ function startGame() {
     document.getElementById("status").innerHTML = "Game Is Loading";
 }
 
+if (game_status == "start") {
+    document.getElementById("status").innerHTML = "Game Is Loaded";
+    //funtion paddleInCanvas call 
+    paddleInCanvas();
+
+    //left paddle
+    fill(250, 0, 0);
+    stroke(0, 0, 250);
+    strokeWeight(0.5);
+    paddle1Y = rightWristY;
+    rect(paddle1X, paddle1Y, paddle1, paddle1Height, 100);
+
+
+    //pc computer paddle
+    fill("#FFA500");
+    stroke("#FFA500");
+    var paddle2y = ball.y - paddle2Height / 2;
+    rect(paddle2Y, paddle2y, paddle2, paddle2Height, 100);
+
+    //function midline call
+    midline();
+
+    //funtion drawScore call 
+    drawScore();
+
+    //function models call  
+    models();
+
+    //function move call which in very important
+    move();
+
+}
+
+
 function gotPoses(results) {
     if (results.length > 0) {
         rightWristY = results[0].pose.rightWrist.y;
